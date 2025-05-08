22110a90bbd173f462b8ab8a16f527d4e295dc9b
Xane735 <sppranav.sharan@gmail.com>
2025-05-08 16:27:46 +0530

    Version 1 of code

--- geo.py
+++ geo.py
@@ -1,7 +1,7 @@
 from pymavlink import mavutil
 import math
 import time
-import azi_elev
+import automate
 
 # Connect as a client (listen for broadcast packets)
 # Use 'tcp:localhost:5762' in the connection string to connect to SITL
@@ -21,45 +21,6 @@ mav.mav.command_long_send(
     0, 0, 0, 0, 0
 )
 
-def loiter(mav):
-    mav.mav.command_long_send(mav.target_system, mav.target_component,
-                                     176, 0, 1, 5, 0, 0, 0, 0, 0)
-    msg = mav.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
-    print("Set to Loiter Mode")
-
-def auto(mav):
-    mav.mav.command_long_send(mav.target_system, mav.target_component,
-                                     176, 0, 1, 3, 0, 0, 0, 0, 0)
-
-    msg = mav.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
-    print("Set to Auto Mode")
-
-def arm(mav):
-    # Arm the drone
-    mav.mav.command_long_send(mav.target_system, mav.target_component,
-                                         400, 0, 1, 0, 0, 0, 0, 0, 0)
-
-    msg = mav.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
-
-def start_mission(mav):
-    # Start the mission by sending MAV_CMD_MISSION_START
-    start_msg = mav.mav.command_long_send(
-                            0, 0,
-                            300,
-                            0,  # Confirmation
-                            0, 0, 0, 0, 0, 0, 0  # Parameters for the command (not used in this case)
-                    )       
-
-    # Send the start mission command
-    mav.mav.send(start_msg)
-
-    test_msg = mav.recv_match(type='MISSION_CURRENT', blocking=True)
-    print(test_msg)
-    print(start_msg)
-    print("Auto mission started!")
 
 def lat_lon_alt_uav(mav):
     # Wait for a GLOBAL_POSITION_INT message from the drone
@@ -100,12 +61,12 @@ def GPS_stream():
 
 
 while True:
-    loiter(mav)
+    automate.loiter(mav)
     time.sleep(3)
-    arm(mav)
+    automate.arm(mav)
     time.sleep(3)
-    auto(mav)
+    automate.auto(mav)
     time.sleep(3)
-    start_mission(mav)
+    automate.start_mission(mav)
     time.sleep(3)
     lat_lon_alt_uav(mav)
