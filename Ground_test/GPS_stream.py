ac3902059ec4633a2b01b4b1a1687d9dd36608fe
Xane735 <sppranav.sharan@gmail.com>
2025-06-04 16:32:39 +0530

    patches

--- Ground_test/GPS_stream.py
+++ Ground_test/GPS_stream.py
@@ -1,15 +1,16 @@
 from pymavlink import mavutil
 
+# Connect to Pixhawk #2 via USB
+usb = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+#usb = mavutil.mavlink_connection('/dev/ttyUSB0', baud=115200)
+usb.wait_heartbeat()
+print("Got heartbeat from Pixhawk #2")
+
 # Connect to Pixhawk #1 via telemetry
 telemetry = mavutil.mavlink_connection('/dev/serial0', baud=57600)
 telemetry.wait_heartbeat()
 print("Got heartbeat from Pixhawk #1")
 
-# Connect to Pixhawk #2 via USB
-usb = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600)
-#usb = mavutil.mavlink_connection('/dev/ttyUSB0', baud=115200)
-usb.wait_heartbeat()
-print("Got heartbeat from Pixhawk #2")
 
 while True:
     msg1 = telemetry.recv_match(type='GLOBAL_POSITION_INT', blocking=False)
